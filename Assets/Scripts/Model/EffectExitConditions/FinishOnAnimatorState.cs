ad6ccf9ba35c369fbc7481fb8991d86138e08f08
LeonardA-L <leonard.allain-launay@insa-lyon.fr>
2018-01-16 18:54:51 +0100

    Add a FinishOnAnimatorState GameEffect exit condition

---
+++ Assets/Scripts/Model/EffectExitConditions/FinishOnAnimatorState.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace vbg
+{
+    public class FinishOnAnimatorState : GameEffectExit
+    {
+        public Animator animator;
+        public string state;
+
+        public override bool AfterUpdate() {
+            if(animator.GetCurrentAnimatorStateInfo(0).IsName(state))
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
