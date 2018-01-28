3816aeeb031560b7a5428bdb0a5eebf93b0f0306
LeonardA-L <leonard@allain-launay.fr>
2018-01-28 16:30:33 +0100

    Add debug display for switches

--- Assets/Scripts/SwitchManager.cs
+++ Assets/Scripts/SwitchManager.cs
@@ -11,6 +11,9 @@ namespace vbg
         public delegate void SwitchCallback(bool switchValue);
         private Dictionary<string, List<SwitchCallback>> switchesPubSub;
 
+        public bool debugSwitches = false;
+        public bool debugValues = false;
+
         protected static SwitchManager instance;
         public static SwitchManager Instance
         {
@@ -26,6 +29,40 @@ namespace vbg
 
             switches = new Dictionary<string, bool>();
             switchesPubSub = new Dictionary<string, List<SwitchCallback>>();
+        }
+
+        void OnGUI()
+        {
+            DisplayDebug();
+        }
+
+        private void DisplayDebug()
+        {
+            if (!Debug.isDebugBuild)
+            {
+                return;
+            }
+
+            int w = Screen.width, h = Screen.height;
+            GUIStyle style = new GUIStyle
+            {
+                alignment = TextAnchor.UpperLeft,
+                fontSize = h * 2 / 100
+            };
+            style.normal.textColor = new Color(1.0f, 1.0f, 1.0f, 1.0f);
+
+            if (debugSwitches)
+            {
+                Rect rect = new Rect(0, 0, w, h * 2 / 100);
+                string text = "";
+
+                foreach (KeyValuePair<string, bool> entry in switches)
+                {
+                    text += entry.Key + ": " + entry.Value + "\n";
+                }
+
+                GUI.Label(rect, text, style);
+            }
         }
 
         public void RegisterSwitchListener(string name, SwitchCallback callback)
