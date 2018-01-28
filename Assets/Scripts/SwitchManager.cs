fe713c1a1c05892ddf503b38fe8af37d6b5937f9
LeonardA-L <leonard@allain-launay.fr>
2018-01-28 16:54:36 +0100

    Add Values system

--- Assets/Scripts/SwitchManager.cs
+++ Assets/Scripts/SwitchManager.cs
@@ -11,6 +11,10 @@ namespace vbg
         public delegate void SwitchCallback(bool switchValue);
         private Dictionary<string, List<SwitchCallback>> switchesPubSub;
 
+        private Dictionary<string, float> floatValues;
+        public delegate void FloatCallback(float floatValue);
+        private Dictionary<string, List<FloatCallback>> floatValuesPubSub;
+
         public bool debugSwitches = false;
         public bool debugValues = false;
 
@@ -29,6 +33,9 @@ namespace vbg
 
             switches = new Dictionary<string, bool>();
             switchesPubSub = new Dictionary<string, List<SwitchCallback>>();
+
+            floatValues = new Dictionary<string, float>();
+            floatValuesPubSub = new Dictionary<string, List<FloatCallback>>();
         }
 
         void OnGUI()
@@ -44,19 +51,41 @@ namespace vbg
             }
 
             int w = Screen.width, h = Screen.height;
-            GUIStyle style = new GUIStyle
-            {
-                alignment = TextAnchor.UpperLeft,
-                fontSize = h * 2 / 100
-            };
-            style.normal.textColor = new Color(1.0f, 1.0f, 1.0f, 1.0f);
+            
 
             if (debugSwitches)
             {
+                GUIStyle style = new GUIStyle
+                {
+                    alignment = TextAnchor.UpperLeft,
+                    fontSize = h * 2 / 100
+                };
+                style.normal.textColor = new Color(1.0f, 1.0f, 1.0f, 1.0f);
+
                 Rect rect = new Rect(0, 0, w, h * 2 / 100);
                 string text = "";
 
                 foreach (KeyValuePair<string, bool> entry in switches)
+                {
+                    text += entry.Key + ": " + entry.Value + "\n";
+                }
+
+                GUI.Label(rect, text, style);
+            }
+
+            if (debugValues)
+            {
+                GUIStyle style = new GUIStyle
+                {
+                    alignment = TextAnchor.UpperRight,
+                    fontSize = h * 2 / 100
+                };
+                style.normal.textColor = new Color(1.0f, 1.0f, 1.0f, 1.0f);
+
+                Rect rect = new Rect(0, 0, w, h * 2 / 100);
+                string text = "";
+
+                foreach (KeyValuePair<string, float> entry in floatValues)
                 {
                     text += entry.Key + ": " + entry.Value + "\n";
                 }
@@ -93,6 +122,44 @@ namespace vbg
                 foreach (SwitchCallback sw in subscribers)
                 {
                     sw(value);
+                }
+            }
+        }
+
+        public void RegisterValueListener(string name, FloatCallback callback)
+        {
+            if (!floatValuesPubSub.ContainsKey(name))
+            {
+                floatValuesPubSub.Add(name, new List<FloatCallback>());
+            }
+            floatValuesPubSub[name].Add(callback);
+        }
+
+        public float? GetValue(string name)
+        {
+
+            if(!floatValues.ContainsKey(name))
+            {
+                return null;
+            }
+
+            float ret;
+            floatValues.TryGetValue(name, out ret);
+            return ret;
+        }
+
+        public void SetValue(string name, float value)
+        {
+            floatValues[name] = value;
+            List<FloatCallback> subscribers = null;
+
+            floatValuesPubSub.TryGetValue(name, out subscribers);
+
+            if (subscribers != null)
+            {
+                foreach (FloatCallback vc in subscribers)
+                {
+                    vc(value);
                 }
             }
         }
