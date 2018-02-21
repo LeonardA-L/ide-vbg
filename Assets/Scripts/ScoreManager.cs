76cd4742723501232a3b290f2c15a10a87aacafb
LeonardA-L <leonard@allain-launay.fr>
2018-02-21 10:03:30 +0100

    Draft ScoreManager

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -6,6 +6,32 @@ namespace vbg
 {
     public class ScoreManager : MonoBehaviour
     {
+        protected static ScoreManager m_instance;
+        public static ScoreManager Instance
+        {
+            get
+            {
+                return m_instance;
+            }
+        }
 
+        private float m_score = 0.0f;
+        private float m_pending = 0.0f;
+
+        void Start()
+        {
+            m_instance = this;
+        }
+
+        public void AddScore(float _diff, bool _hold = false)
+        {
+            m_pending += _diff;
+
+            if (!_hold)
+            {
+                m_score += m_pending;
+                m_pending = 0.0f;
+            }
+        }
     }
 }
