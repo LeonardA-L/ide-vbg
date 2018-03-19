e42bb70da311a769f983f373ba44ea0e64d72452
LeonardA-L <leonard@allain-launay.fr>
2018-03-19 16:43:05 +0100

    Add VFX system

---
+++ Assets/Scripts/VFX.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace vbg
+{
+    public class VFX : MonoBehaviour
+    {
+
+        private List<ParticleSystem> systems = new List<ParticleSystem>();
+
+        private bool hasEnded = false;
+        private Transform follow = null;
+        private Vector3 followOffset;
+        private bool followRotation = false;
+
+        public float destroyTimeout = 5.0f;
+
+        private void FindParticles(Transform _tr)
+        {
+            ParticleSystem system = _tr.GetComponent<ParticleSystem>();
+            if (system != null)
+            {
+                systems.Add(system);
+            }
+
+            foreach(Transform child in _tr)
+            {
+                FindParticles(child);
+            }
+        }
+
+        // Use this for initialization
+        void Start()
+        {
+            FindParticles(transform);
+        }
+
+        public void SetFXData(bool _followRotation = false, Transform _follow = null)
+        {
+            follow = _follow;
+            followRotation = _followRotation;
+
+            if (follow != null)
+            {
+                followOffset = transform.position - follow.position;
+            }
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if(hasEnded)
+            {
+                destroyTimeout -= Time.deltaTime;
+                if(destroyTimeout <= 0.0f)
+                {
+                    Destroy(gameObject);
+                }
+            }
+
+            if(follow != null)
+            {
+                transform.position = follow.position + followOffset;
+
+                if(followRotation)
+                {
+                    transform.rotation = follow.rotation;
+                }
+            }
+        }
+
+        public void End()
+        {
+            hasEnded = true;
+            foreach(ParticleSystem system in systems)
+            {
+                system.Stop();
+            }
+        }
+    }
+}
---
+++ Assets/VFXSpawner.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace vbg
+{
+    public class VFXSpawner : MonoBehaviour
+    {
+        public GameObject vfxPrefab;
+        public bool followParentPosition = false;
+        public bool followParentRotation = false;
+
+        private VFX vfx;
+
+        // Use this for initialization
+        void Start()
+        {
+            GameObject go = GameObject.Instantiate(vfxPrefab);
+            Transform tr = go.transform;
+
+            vfx = go.GetComponent<VFX>();
+
+            tr.position = transform.parent.position + transform.localPosition;
+            tr.rotation = transform.parent.rotation;
+
+            vfx.SetFXData(followParentRotation, followParentPosition ? transform.parent : null);
+        }
+
+        private void OnDestroy()
+        {
+            if(vfx != null)
+            {
+                vfx.End();
+            }
+        }
+    }
+}
