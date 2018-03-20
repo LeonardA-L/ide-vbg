98f3484a2fcbfae9956ab21d3f179d0c5da04bdc
LeonardA-L <leonard@allain-launay.fr>
2018-03-20 17:06:47 +0100

    Generate GUIDs for credits

---
+++ Assets/Scripts/Credit.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[ExecuteInEditMode]
+public class Credit : MonoBehaviour {
+    private string creditStringID = null;
+    private int creditID = -1;
+
+#if UNITY_EDITOR
+    void Awake()
+    {
+        Debug.Log("Editor causes this Awake");
+        GenerateID();
+    }
+#endif
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+#if UNITY_EDITOR
+        Debug.Log(creditStringID);
+        GenerateID();
+#endif
+    }
+
+    private void GenerateID()
+    {
+        if(creditStringID == null || creditStringID == "")
+        {
+            Credit[] credits = FindObjectsOfType(typeof(Credit)) as Credit[];
+
+            int max = -1;
+
+            foreach(Credit credit in credits)
+            {
+                if(credit == this)
+                {
+                    continue;
+                }
+
+                max = Mathf.Max(max, credit.creditID);
+            }
+
+            max++;
+
+            Scene scene = SceneManager.GetActiveScene();
+            creditStringID = scene.name + "_" + max;
+            creditID = max;
+        }
+    }
+}
