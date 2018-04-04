750c1a29a0fa437450278602f7032def12b0a940
LeonardA-L <leonard@allain-launay.fr>
2018-04-04 19:23:50 +0200

    Move scripts around

--- Assets/Scripts/MainMenuController.cs
+++ Assets/Scripts/MainMenuController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class MenuManager : MonoBehaviour {
+public class MainMenuController : MonoBehaviour {
     public Animator cameraAnimator;
 
     public List<Button> heads;
