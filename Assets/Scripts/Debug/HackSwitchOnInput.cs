977f2b9d49ce88344a8675201c84ca63ef3758aa
LeonardA-L <leonard@allain-launay.fr>
2018-04-06 22:32:06 +0200

    Add hack to autosolve puzzle

---
+++ Assets/Scripts/Debug/HackSwitchOnInput.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace vbg
+{
+    public class HackSwitchOnInput : MonoBehaviour
+    {
+
+        public string keyName = "";
+        public string switchName = "";
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Input.GetKey(keyName))
+            {
+                SwitchManager.Instance.SetSwitch(switchName, true);
+            }
+        }
+    }
+}
