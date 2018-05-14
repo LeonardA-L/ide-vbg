61ed2748a6c5b577d48d849ef3582e7abec010fa
LeonardA-L <leonard@allain-launay.fr>
2018-05-14 18:34:52 +0200

    Draft rotation of integrated UI

---
+++ Assets/Scripts/CopyRotation3DTo2D.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace vbg
+{
+    public class CopyRotation3DTo2D : MonoBehaviour
+    {
+        public Transform from3D;
+        public RectTransform to2D;
+
+        // Update is called once per frame
+        void Update()
+        {
+            to2D.localRotation = Quaternion.Euler(0, 0.0f, Quaternion.ToEulerAngles(from3D.rotation).y * Mathf.Rad2Deg);
+        }
+    }
+}
