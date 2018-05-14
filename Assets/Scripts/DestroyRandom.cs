6fcd3e970ab6defe4fbe751018bd2cc34ea95ea6
LeonardA-L <leonard@allain-launay.fr>
2018-05-14 10:35:47 +0200

    Add random chance for skeletons bones to disappear

---
+++ Assets/Scripts/DestroyRandom.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyRandom : MonoBehaviour {
+
+    [Range(0.0f, 1.0f)]
+    public float probability = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+        float rand = Random.Range(0.0f, 1.0f);
+        if(rand < probability)
+        {
+            Destroy(gameObject);
+        }
+	}
+}
