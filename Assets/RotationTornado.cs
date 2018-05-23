86dced3d2daeb37f04077a2c3892b11d1e02a3cc
AntoineBlz <33094192+AntoineBlz@users.noreply.github.com>
2018-05-23 17:41:13 +0200

    FX - Add ground for wall fx work

---
+++ Assets/RotationTornado.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class RotationTornado : MonoBehaviour
+{
+    public float rotSpeed = 50;
+    void Update()
+    {
+        transform.Rotate(Vector3.up * Time.deltaTime * rotSpeed, Space.World);
+    }
+}
