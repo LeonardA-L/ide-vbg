fb33295f334f4760383eaef3d2a5a36321b2aec3
LeonardA-L <leonard@allain-launay.fr>
2018-06-05 17:55:32 +0200

    Make a single wwise session for all scenes

---
+++ Assets/Scripts/Debug/DestroyOnProd.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOnProd : MonoBehaviour {
+	void Start () {
+        if (!Debug.isDebugBuild)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
