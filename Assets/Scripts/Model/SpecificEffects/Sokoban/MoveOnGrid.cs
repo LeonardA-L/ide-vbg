a54baf43ffcf2903e405ecb78fffbbe4086cc549
LeonardA-L <leonard@allain-launay.fr>
2018-06-05 22:37:34 +0200

    Integrate Sokoban puzzle

--- Assets/Scripts/Model/SpecificEffects/Sokoban/MoveOnGrid.cs
+++ Assets/Scripts/Model/SpecificEffects/Sokoban/MoveOnGrid.cs
@@ -9,6 +9,8 @@ namespace vbg
         private Vector3 goal;
         public bool lockPos = false;
         public Rigidbody rb;
+        private float temp = 0.0f;
+        private float wait = 0.3f;
 
         // Use this for initialization
         void Start()
@@ -21,6 +23,8 @@ namespace vbg
         void FixedUpdate()
         {
             transform.position = Vector3.Lerp(transform.position, goal, 0.15f);
+            temp -= Time.fixedDeltaTime;
+
             //rb.MovePosition(Vector3.Lerp(transform.position, goal, 0.3f));
             /*if (lastPosition != transform.position)
             {
@@ -38,22 +42,62 @@ namespace vbg
 
         }
 
+        public Vector3 FindClosest(Vector3 _from)
+        {
+            Vector3 gridForward = (new Vector3(1, 0, 1)).normalized;
+            Vector3 gridBackward = -gridForward;
+            Vector3 gridSide = (new Vector3(1, 0, -1)).normalized;
+            Vector3 gridBackSide = -gridSide;
+
+            Vector3 ret = gridForward;
+            float max = -10;
+
+            if(Vector3.Dot(_from, gridForward) > max)
+            {
+                max = Vector3.Dot(_from, gridForward);
+                ret = gridForward;
+            }
+            if (Vector3.Dot(_from, gridBackward) > max)
+            {
+                max = Vector3.Dot(_from, gridBackward);
+                ret = gridBackward;
+            }
+            if (Vector3.Dot(_from, gridSide) > max)
+            {
+                max = Vector3.Dot(_from, gridSide);
+                ret = gridSide;
+            }
+            if (Vector3.Dot(_from, gridBackSide) > max)
+            {
+                max = Vector3.Dot(_from, gridBackSide);
+                ret = gridBackSide;
+            }
+
+
+            return ret;
+        }
+
         private void OnCollisionEnter(Collision collision)
         {
             if (lockPos)
                 return;
+            if (temp > 0)
+                return;
             if(collision.transform.tag == GameManager.Constants.TAG_PLAYER)
             {
+
                 Vector3 disp = transform.position - collision.transform.position;
                 disp.y = 0;
-                if (Mathf.Abs(disp.x) > Mathf.Abs(disp.z))
+                /*if (Mathf.Abs(disp.x) > Mathf.Abs(disp.z))
                 {
                     disp.z = 0;
                 }
                 else
                 {
                     disp.x = 0;
-                }
+                }*/
+
+                disp = FindClosest(disp);
 
                 disp.Normalize();
 
@@ -62,8 +106,11 @@ namespace vbg
                 Debug.DrawLine(transform.position, transform.position + disp * 2, Color.blue, 2.0f);
                 Debug.Log(hits.Length);
 
-                if(hits.Length == 0)
+                if (hits.Length == 0)
+                {
                     goal += disp * 2;
+                    temp = wait;
+                }
             }
         }
 
