548ef14681b16adcb093b30d0eaaafd084ac4355
LeonardA-L <leonard@allain-launay.fr>
2018-06-08 09:46:48 +0200

    Add teleport hack for puzzle

---
+++ Assets/Scripts/HackTeleportPuzzleCrane.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace vbg {
+    public class HackTeleportPuzzleCrane : MonoBehaviour {
+        public List<Transform> hotspots = new List<Transform>();
+
+
+        public void HackTeleport()
+        {
+            List<VBGCharacterController> players = PlayerManager.Instance.GetAllPlayersInGame();
+            int i = 0;
+            foreach(VBGCharacterController p in players)
+            {
+                p.transform.position = hotspots[i].position;
+                i++;
+            }
+        }
+    }
+}
