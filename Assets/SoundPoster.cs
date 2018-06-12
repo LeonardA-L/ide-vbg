e8a0d76da1f1eea1be9a195759932120503913f5
LeonardA-L <leonard@allain-launay.fr>
2018-06-12 03:11:56 +0200

    Add sound to crane

---
+++ Assets/SoundPoster.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace vbg
+{
+    public class SoundPoster : MonoBehaviour
+    {
+
+        public void PostAudioEvent(string _eventName)
+        {
+            if (_eventName != null && _eventName != "")
+                SoundManager.Instance.PostEvent(_eventName, this.gameObject);
+        }
+    }
+}
