d6198ccf590992a5aaed199826c0f1464efa2baa
LeonardA-L <leonard@allain-launay.fr>
2018-06-13 16:28:24 +0200

    Presentation with trailer

---
+++ Assets/Scripts/CameraFreeTrigger.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace vbg
+{
+    public class CameraFreeTrigger : MonoBehaviour
+    {
+        private void OnTriggerEnter(UnityEngine.Collider _collider)
+        {
+            if (_collider.gameObject.tag != GameManager.Constants.TAG_PLAYER)
+                return;
+
+            CameraManager.Instance.SetFreeSettings();
+        }
+    }
+}
