132fc3534ef628b51376452b6f02262aeea4f9b3
LeonardA-L <leonard@allain-launay.fr>
2018-06-17 16:35:36 +0200

    Add editor script to count Dynamic objects and meshColliders

---
+++ Assets/Scripts/Debug/DynamicCount.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace vbg
+{
+    [ExecuteInEditMode]
+    public class DynamicCount : MonoBehaviour
+    {
+#if UNITY_EDITOR
+        // Use this for initialization
+        void Awake ()
+        {
+            var dynamics = FindObjectsOfType<Dynamic>();
+            Debug.Log("Dynamic : " + dynamics.Length);
+            var meshCo = FindObjectsOfType<MeshCollider>();
+            Debug.Log("MeshCollider : " + meshCo.Length);
+        }
+#endif
+    }
+}
