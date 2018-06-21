707b92c27460294c2bd81e7eb8d268cd22ee7f45
LeonardA-L <leonard@allain-launay.fr>
2018-06-21 22:14:04 +0200

    Init Ultimate Burger

---
+++ Assets/UltimateBurger/Scripts/UBCharacterController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using vbg;
+
+namespace ub
+{
+    public class UBCharacterController : MonoBehaviour
+    {
+        public struct Request
+        {
+            public float LThruster;
+            public float RThruster;
+        }
+
+        private UBUserInput inputs;
+        private Rigidbody rb;
+        public float maxThruster = 6;
+        public float shift = 0.5f;
+        public float speedToShift= 0.01f;
+
+        private Vector3 thrustDirection = new Vector3(0, 0, 1);
+
+        // Use this for initialization
+        void Start()
+        {
+            inputs = GetComponent<UBUserInput>();
+            rb = GetComponent<Rigidbody>();
+        }
+
+        void FixedUpdate()
+        {
+            Request req = inputs.GetRequest();
+
+            Debug.Log(req.LThruster + " - " + req.RThruster);
+
+            float shiftFactor = Mathf.Abs(shift);
+
+            rb.AddTorque(transform.up * shiftFactor * -req.LThruster, ForceMode.Acceleration);
+            rb.AddTorque(transform.up * shiftFactor * req.RThruster, ForceMode.Acceleration);
+
+            rb.AddForce((req.LThruster + req.RThruster) * maxThruster * TransformToWorld(thrustDirection));
+        }
+
+        Vector3 TransformToWorld(Vector3 from)
+        {
+            return from.x * transform.right
+                + from.y * transform.up
+                + from.z * transform.forward;
+        }
+    }
+}
---
+++ Assets/UltimateBurger/Scripts/UBUserInput.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using vbg;
+
+namespace ub
+{
+    public class UBUserInput : MonoBehaviour
+    {
+        public int controllerID = -1;
+
+
+        public UBCharacterController.Request GetRequest()
+        {
+            UBCharacterController.Request req = new UBCharacterController.Request();
+
+            float LeftThruster = Input.GetAxis("Modifier" + GetControllersuffix());
+            float RightThruster = Input.GetAxis("Attack_ALT" + GetControllersuffix());
+
+            req.LThruster = LeftThruster;
+            req.RThruster = -RightThruster;
+
+            return req;
+        }
+
+        public void SetController(int _controllerID)
+        {
+            controllerID = _controllerID;
+        }
+        string GetControllersuffix()
+        {
+            return "_P" + controllerID;
+        }
+    }
+}
